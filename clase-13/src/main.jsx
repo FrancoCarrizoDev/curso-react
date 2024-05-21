8028a68e45c24a0c8753b48a99b8f033437d06c4
FrancoCarrizoDev <francoadrianc@gmail.com>
2024-05-21 20:48:44 -0300

    Update ESLint configuration and add Firebase environment variables

--- clase-13/src/main.jsx
+++ clase-13/src/main.jsx
@@ -5,13 +5,15 @@ import "./index.css";
 
 import { initializeApp } from "firebase/app";
 
+console.log(import.meta.env);
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD6xrnujt3tjRktz56BZh1FxtMq3sWFzmw",
-  authDomain: "react-54120.firebaseapp.com",
-  projectId: "react-54120",
-  storageBucket: "react-54120.appspot.com",
-  messagingSenderId: "556667336953",
-  appId: "1:556667336953:web:a7e85123f8355af46d8a28",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
